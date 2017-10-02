bbbc9e8d632b0566573417db31f386c1d0230cc0
behzadnabavi <32080535+behzadnabavi@users.noreply.github.com>
2017-10-01 21:04:12 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,4 @@
 png("plot1.png",width= 6, height = 6,units= "in", res= 80)
 tab1<-read.table("household_power_consumption.txt",skip=66637, nrows = 2880, sep = ";", col.names = c("Date","Hour","Active Power", "Reactive Power", "Voltage","Intensity", "S M 1", "S M 2", "S M 3"))
 hist(tab1$Active.Power,col = "red", xlab = "Global Active Power (killowatts)", main = "Global Active Power")
-#dev.copy(png,'plot1.png')
 dev.off()
